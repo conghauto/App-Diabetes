a17a8c887cadc5bf032c1e3a6156dcf1f71e3eb3
hautodp <37023303+hautodp@users.noreply.github.com>
2020-11-09 23:59:44 +0700

    add UI

---
+++ lib/screens/advice/food_recommend_screen.dart
@@ -0,0 +1,35 @@
+import 'package:diabetesapp/size_config.dart';
+import 'package:flutter/material.dart';
+
+class FoodRecommendScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+Widget _myListView(BuildContext context) {
+
+  // backing data
+  final europeanCountries = ['Albania', 'Andorra', 'Armenia', 'Austria',
+    'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria',
+    'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland',
+    'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland',
+    'Italy', 'Kazakhstan', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania',
+    'Luxembourg', 'Macedonia', 'Malta', 'Moldova', 'Monaco', 'Montenegro',
+    'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia',
+    'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden',
+    'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom', 'Vatican City'];
+
+  return ListView.builder(
+    itemCount: europeanCountries.length,
+    itemBuilder: (context, index) {
+      return Card(
+        child: ListTile(
+          title: Text(europeanCountries[index]),
+        ),
+      );
+    },
+  );
+
+}
