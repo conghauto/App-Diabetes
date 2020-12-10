062c22359bc8d8b3ae7fd960279a94f00e99707f
toantq.uit <quoctoan0629@gmail.com>
2020-12-10 21:05:59 +0700

    add foods database and APIs

---
+++ Database_PHP/api/getLunch.php
@@ -0,0 +1,108 @@
+<?php
+    header("Content-type: application/json; charset=utf-8");
+    include("config.php");
+
+    $userID = $_POST['userID'];
+    $intensity = "light";
+    $row = mysqli_query($con,"SELECT height, gender FROM personalinfos WHERE userID='".$userID."' LIMIT 1");
+    $r1 = mysqli_fetch_assoc($row);
+    $height = $r1['height'];
+    $gender = $r1['gender'];
+
+    // get type of activity from user
+    $day = date("j");
+    $month = date("n");
+    $year = date("Y");
+    $rowactivity = mysqli_query($con,"SELECT indexMET FROM activities WHERE userID ='".$userID."' and DAY(measureTime) ='".$day."' AND MONTH(measureTime) ='".$month."' and YEAR(measureTime) ='".$year."' LIMIT 1");
+    $resultActivity = mysqli_fetch_assoc($rowactivity);
+    if ($resultActivity != null) {
+        $indexMet = $resultActivity['indexMET'];
+        if ($indexMet > 3 && $indexMet < 6) {
+            $intensity = "moderate";
+        }
+        if ($indexMet >= 6) {
+            $intensity = "vigorous";
+        }
+    }
+    // Tính cân nặng lý tưởng: CNLT = (chieu cao - 100)*0.9
+    if($height!=null&&$gender!=null){
+        $idealWeight = ($height*100 -100)*0.9;
+        // Lượng calo cần: 
+        // light: nhẹ, moderate: vừa phải, vigorous: mạnh
+        if($gender==1){
+            switch($intensity){
+                case "light":
+                    $calo = 30 * $idealWeight;
+                    break;
+                case "moderate":
+                    $calo = 35 * $idealWeight;
+                    break;
+                case "vigorous":
+                    $calo = 45 * $idealWeight;
+                    break;
+                default:
+                    $calo = 30 * $idealWeight; 
+            }
+        }else{
+            switch($intensity){
+                case "light":
+                    $calo = 25 * $idealWeight;
+                    break;
+                case "moderate":
+                    $calo = 30* $idealWeight;
+                    break;
+                case "vigorous":
+                    $calo = 40 * $idealWeight;
+                    break;
+                default:
+                    $calo = 25 * $idealWeight; 
+            }
+        }
+    }
+
+
+
+    $result = array();
+
+    //bữa trưa chiếm 25% lượng calo
+    $lunch = $calo*25;
+
+    $queryBreakfast="SELECT *FROM foods WHERE meal='lunch' and calo<'".$lunch."' ";
+    $data = mysqli_query($con,$queryBreakfast);
+    
+    while($row=mysqli_fetch_assoc($data)){
+        array_push($result,new Food(
+            $row['id'],
+            $row['name'],
+            $row['amount'],
+            $row['unit'],
+            $row['calo'],
+            $row['protein'],
+            $row['lipid'],
+            $row['carb'],
+            $row['cellulose'],
+            $row['meal'],
+            $row['stateBG'])    
+        );
+    }
+
+    echo json_encode($result);
+
+    class Food{
+        public function Food($id,$name,$amount,$unit,$calo,$protein,$lipid,$carb,$cellulose,$meal,$stateBG){
+            $this->id=$id;
+            $this->name=$name;
+            $this->amount=$amount;
+            $this->unit=$unit;
+            $this->calo=$calo;
+            $this->protein=$protein;
+            $this->lipid=$lipid;
+            $this->carb=$carb;
+            $this->cellulose=$cellulose;
+            $this->meal=$meal;
+            $this->stateBG=$stateBG;
+        }
+    }
+
+
+?>
--- Database_PHP/foods.sql
+++ Database_PHP/foods.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2020 at 06:03 PM
+-- Generation Time: Dec 10, 2020 at 02:52 PM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,24 +29,96 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `foods` (
   `id` int(11) NOT NULL,
-  `name` varchar(1000) CHARACTER SET utf8 NOT NULL,
-  `image` varchar(2000) DEFAULT NULL,
-  `ingredient` varchar(5000) CHARACTER SET utf8 NOT NULL,
-  `recipe` varchar(5000) CHARACTER SET utf8 NOT NULL,
-  `benefit` varchar(5000) CHARACTER SET utf8 NOT NULL,
-  `groupID` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `name` varchar(1000) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `unit` varchar(50) NOT NULL,
+  `calo` float NOT NULL,
+  `protein` float NOT NULL,
+  `lipid` float NOT NULL,
+  `carb` float NOT NULL,
+  `cellulose` float NOT NULL,
+  `meal` varchar(100) NOT NULL,
+  `stateBG` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `foods`
 --
 
-INSERT INTO `foods` (`id`, `name`, `image`, `ingredient`, `recipe`, `benefit`, `groupID`) VALUES
-(1, 'Nấm xào cải xanh và bắp non', 'https://yt.cdnxbvn.com/medias/uploads/35/35536-mon-an-cho-nguoi-benh-tieu-duong-1.jpg', '350g cải xanh: làm sạch và thái khúc\r\n6 tai nấm hương tươi: cắt bỏ cuống, ngâm qua nước muối pha loãng\r\n50g bắp non\r\n1 củ hành tím: lột vỏ và băm nhỏ\r\nGia vị: 1/3 muỗng cà phê muối, 1/2 muỗng cà phê bột ngọt và ít dầu ăn', 'Phi thơm hành tím với ít dầu, cho nấm vào xào. \r\nKhi nấm chuyển màu chín, cho tiếp phần bắp non và rau cải xanh vào xào cùng. \r\nNêm lại gia vị và tắt bếp.', 'Dùng cho người mắc bệnh tiểu đường kèm bệnh động mạch vành, mỡ máu cao hoặc cao huyết áp', 1),
-(2, 'Nhộng tằm xào lá chanh', 'https://yt.cdnxbvn.com/medias/uploads/35/35537-mon-an-cho-nguoi-benh-tieu-duong-2.jpg', '100g nhộng tằm: rửa sạch và xóc ráo nước.\r\nVài lá chanh tươi: rửa sạch và thái sợi\r\nGia vị: 1/2 muỗng cà phê muối, 1/2 muỗng cá phê nước mắm ngon, 1/2 muỗng bột ngọt và 2 muỗng cà phê dầu ăn.', 'Làm nóng dầu, sau đó cho nhộng tằm vào xào săn với lửa nhỏ. \r\nKhi nhộng chín, nêm nếm gia vị và rắc lá chanh vào đảo đều. \r\nTắt bếp sau khoảng 3 phút.', 'Thích hợp với mọi loại bệnh tiểu đường vì nó có tác dụng giảm đường huyết hiệu quả.', 1),
-(3, 'Thịt heo xào hành tây', 'https://yt.cdnxbvn.com/medias/uploads/35/35538-mon-an-cho-nguoi-benh-tieu-duong-3.jpg', '2 củ hành tây: lột vỏ và thái múi cau\r\n100g thịt nạc: thái mỏng\r\nĐầu hành lá: rửa sạch và băm nhỏ\r\nGia vị: 1 muỗng cà phê tương, 1/2 muỗng cà phê muối, 1/2 muỗng cà phê bột ngọt và 2 muỗng canh dầu', 'Phi thơm đầu hành với ít dầu nóng, sau đó cho thịt heo vào xào săn. \r\nKhi thịt chuyển săn, cho tiếp phần hành tây vào đảo đều. \r\nNấu khoảng 3 phút, nêm nếm gia vị và tiếp tục đảo đều thêm lần nữa trước khi tắt bếp.', 'Món này giúp ích thận, hạ đường huyết, phù hợp để dùng cho người mắc bị tiểu đường có các triệu chứng nóng gan hoặc mắc kèm bệnh thận, bàng quang.', 1),
-(4, 'Nấm rơm xào thịt nạc', 'https://yt.cdnxbvn.com/medias/uploads/35/35553-mon-an-cho-nguoi-benh-tieu-duong-19.jpg', '300g nấm rơm tươi: rửa sạch, ngâm qua nước muối pha loãng và để ráo\r\n50g thịt nạc heo: thái nhỏ\r\n1 củ hành tím băm nhỏ\r\nGia vị: 1/2 muỗng cà phê muối, ½ muỗng cà phê bột ngọt và 2 muỗng cà phê dầu mè', 'Phi hành tím cho dậy thơm, sau đó trút phần thịt vào xào săn. Kế đến, cho nấm vào xào cùng. Sau khoảng 10 phút, nấm thấm vị thịt, nêm nếm lại gia vị và tắt bếp.', 'bổ khí dưỡng huyết, tăng sức đề kháng, thích hợp dùng người mắc bệnh tiểu đường kèm khí huyết hư nhược hoặc gan nhiễm mỡ.', 1),
-(5, 'Thịt heo nạc xào cần tây', 'https://yt.cdnxbvn.com/medias/uploads/35/35539-mon-an-cho-nguoi-benh-tieu-duong-4.jpg', '50g thịt heo: rửa sạch và thái nhuyễn\r\n300g rau cần tây: cắt bỏ rễ, rửa sạch và cắt khúc\r\n1 quả trứng gà\r\n15g khoai mài khô: rửa qua nước và để ráo\r\nVài lát gừng tươi thái nhuyễn\r\n10 bột năng\r\n1 củ hành tím băm nhỏ\r\nGia vị: 1 muỗng cà phê muối, 1 muỗng cà phê bột ngọt và 2 muỗng cà phê dầu ăn.', 'Cho khoai mài vào chảo với ít nóng và xào đến đi chín mềm, sau đó cho cần tây và gừng vào đảo đều. Nêm với ít muối, bột ngọt cho vừa miệng trước khi tắt bếp.\r\n- Trộn đều phần thịt heo, bột năng với trứng gà và ít muối.\r\n- Khử dầu nóng với ít hành, sau đó cho hỗn hợp thịt vào đảo đều. Khi thịt chín, trút phần khoai đã xào vào đảo đều.', 'Giúp người mắc bệnh tiểu đường hạ đường huyết, đồng thời giúp hạ huyết áp đi kèm với bệnh.', 1);
+INSERT INTO `foods` (`id`, `name`, `amount`, `unit`, `calo`, `protein`, `lipid`, `carb`, `cellulose`, `meal`, `stateBG`) VALUES
+(1, 'Phở gà', 1, 'tô', 483, 17.9, 59.3, 21.3, 2.28, 'breakfast', 'high'),
+(2, 'Há cảo', 6, 'viên', 363, 12.2, 56, 7.4, 0.75, 'breakfast', 'high'),
+(3, 'Bánh mì thịt', 1, 'ổ', 399, 15.1, 13.7, 53.8, 0.59, 'breakfast', 'high'),
+(4, 'Bánh canh thịt heo', 1, 'tô', 322, 12.8, 8.5, 48.5, 1, 'breakfast', 'high'),
+(5, 'Hoành thánh', 1, 'tô', 248, 7.4, 31.7, 12.3, 1.26, 'breakfast', 'high'),
+(6, 'Bánh cuốn', 1, 'đĩa', 590, 25.7, 25.6, 64.3, 1.26, 'breakfast', 'high'),
+(7, 'Cháo đậu đỏ', 1, 'chén', 322, 11.8, 43.7, 10.6, 2.42, 'breakfast', 'high'),
+(8, 'Bún bò', 1, 'tô', 479, 18.4, 16, 65.3, 3.3, 'breakfast', 'high'),
+(9, 'Khoai lang', 1, 'củ', 131, 1.4, 0.3, 30.6, 0.9, 'breakfast', 'high'),
+(10, 'Bún riêu cua', 1, 'tô', 482, 16.8, 66, 16.5, 3.4, 'breakfast', 'high'),
+(11, 'Bánh Flan', 1, 'cái', 66, 1.7, 1.6, 11.3, 0.05, 'snack', 'high'),
+(12, 'Bánh quy bơ', 1, 'cái', 38, 0.9, 0.5, 7.5, 0.05, 'snack', 'high'),
+(13, 'Bắp luộc', 1, 'trái', 192, 4.5, 2.5, 37.8, 1.38, 'snack', 'high'),
+(14, 'Sữa chua', 1, 'hộp', 137, 4, 21.6, 3.8, 0, 'snack', 'high'),
+(15, 'Bưởi', 1, 'múi', 8, 0, 5.1, 0.1, 0.72, 'snack', 'high'),
+(16, 'Quýt', 1, 'trái', 28, 0, 6.4, 0.6, 0.44, 'snack', 'high'),
+(17, 'Mãng cầu xiêm', 1, 'miếng', 40, 1.4, 0, 8.6, 1.52, 'snack', 'high'),
+(18, 'Vú sữa', 1, 'trái', 83, 2, 0, 18.5, 4.53, 'snack', 'high'),
+(19, 'Thanh long', 1, 'trái', 225, 7.3, 0, 49, 10.13, 'snack', 'high'),
+(20, 'Cháo yến mạch', 1, 'tô', 194, 8, 3, 33, 5, 'breakfast', 'high'),
+(21, 'Cháo bắp mộc nhỉ', 1, 'tô', 116, 5, 1, 25, 3, 'breakfast', 'high'),
+(22, 'Cơm', 1, 'chén', 200, 4, 0, 44, 0, 'lunch', 'high'),
+(23, 'Bún mộc', 1, 'tô', 514, 28, 19, 56, 2, 'lunch', 'high'),
+(24, 'Mì quảng', 1, 'tô', 541, 22, 20, 67, 2, 'lunch', 'high'),
+(25, 'Cháo bí đao', 1, 'tô', 126, 3, 0, 27, 1, 'lunch', 'high'),
+(26, 'Cháo yến mạch', 1, 'tô', 194, 8, 3, 33, 5, 'lunch', 'high'),
+(27, 'Cháo bột bắp', 1, 'tô', 370, 8, 3, 77, 4, 'lunch', 'high'),
+(28, 'Cháo bắp mộc nhỉ', 1, 'tô', 116, 5, 1, 25, 3, 'lunch', 'high'),
+(29, 'Thịt kho trứng', 1, 'đĩa', 315, 19, 22, 7, 0, 'lunch', 'high'),
+(30, 'Thịt bò xào nấm rơm', 1, 'đĩa', 152, 13, 9, 2, 0, 'lunch', 'high'),
+(31, 'Cá chép chưng tương', 1, 'con', 156, 6.6, 7, 16, 0, 'lunch', 'high'),
+(32, 'Cá hú kho', 1, 'lát cá', 184, 15, 9, 8, 0, 'lunch', 'high'),
+(33, 'Canh gà bí đao', 1, 'chén', 211, 21, 13, 4, 2, 'lunch', 'high'),
+(34, 'Hành tây xào thịt lợn', 1, 'đĩa', 109, 10, 3, 9, 1, 'lunch', 'high'),
+(35, 'Nấm hương đậu phụ', 1, 'tô', 229, 25.25, 11, 9, 12, 'lunch', 'high'),
+(36, 'Dưa chuột trộn sứa', 1, 'đĩa', 135, 9, 1, 15, 3, 'lunch', 'high'),
+(37, 'Lươn nấu (200g)', 1, 'đĩa', 188, 40, 3, 0, 0, 'lunch', 'high'),
+(38, 'Bí đỏ nấu chay (300g)', 1, 'chén', 120, 3, 0, 30, 3, 'lunch', 'high'),
+(39, 'Mướp đắng xào thịt', 1, 'đĩa', 103, 11, 3, 8, 6, 'lunch', 'high'),
+(40, 'Canh bầu', 1, 'chén', 30, 2, 1, 1, 0, 'lunch', 'high'),
+(41, 'Canh cải ngọt', 1, 'chén', 30, 2, 1, 1, 0, 'lunch', 'high'),
+(42, 'Canh bí đao', 1, 'chén', 29, 2, 1, 1, 0, 'lunch', 'high'),
+(43, 'Canh rau dền', 1, 'chén', 22, 2, 0, 0, 0, 'lunch', 'high'),
+(44, 'Nước củ hành', 1, 'chén', 40, 1, 0, 9, 1, 'lunch', 'high'),
+(45, 'Canh bí đao, cà chua', 1, 'chén', 43, 1, 0, 9, 2, 'lunch', 'high'),
+(46, 'Canh xanh bạch ngọc', 1, 'chén', 40, 3, 0, 7, 3, 'lunch', 'high'),
+(47, 'Canh bí xanh thịt nạc', 1, 'chén', 103, 12, 3, 6, 2, 'lunch', 'high'),
+(48, 'Cơm', 1, 'chén', 200, 4, 0, 44, 0, 'dinner', 'high'),
+(49, 'Bún mộc', 1, 'tô', 514, 28, 19, 56, 2, 'dinner', 'high'),
+(50, 'Mì quảng', 1, 'tô', 541, 22, 20, 67, 2, 'dinner', 'high'),
+(51, 'Cháo bí đao', 1, 'tô', 126, 3, 0, 27, 1, 'dinner', 'high'),
+(52, 'Cháo yến mạch', 1, 'tô', 194, 8, 3, 33, 5, 'dinner', 'high'),
+(53, 'Cháo bột bắp', 1, 'tô', 370, 8, 3, 77, 4, 'dinner', 'high'),
+(54, 'Cháo bắp mộc nhỉ', 1, 'tô', 116, 5, 1, 25, 3, 'dinner', 'high'),
+(55, 'Lươn nấu (200g)', 1, 'đĩa', 188, 40, 3, 0, 0, 'dinner', 'high'),
+(56, 'Bí đỏ nấu chay (300g)', 1, 'chén', 120, 3, 0, 30, 3, 'dinner', 'high'),
+(57, 'Canh gà bí đao', 1, 'chén', 211, 21, 13, 4, 2, 'dinner', 'high'),
+(58, 'Hành tây xào thịt lợn', 1, 'đĩa', 109, 10, 3, 9, 1, 'dinner', 'high'),
+(59, 'Nấm hương đậu phụ', 1, 'tô', 229, 25.25, 11, 9, 12, 'dinner', 'high'),
+(60, 'Dưa chuột trộn sứa', 1, 'đĩa', 135, 9, 1, 15, 3, 'dinner', 'high'),
+(61, 'Thịt kho trứng', 1, 'đĩa', 315, 19, 22, 7, 0, 'dinner', 'high'),
+(62, 'Thịt bò xào nấm rơm', 1, 'đĩa', 152, 13, 9, 2, 0, 'dinner', 'high'),
+(63, 'Cá chép chưng tương', 1, 'con', 156, 6.6, 7, 16, 0, 'dinner', 'high'),
+(64, 'Cá hú kho', 1, 'lát cá', 184, 15, 9, 8, 0, 'dinner', 'high'),
+(65, 'Mướp đắng xào thịt', 1, 'đĩa', 103, 11, 3, 8, 6, 'dinner', 'high'),
+(66, 'Canh bầu', 1, 'chén', 30, 2, 1, 1, 0, 'dinner', 'high'),
+(67, 'Canh cải ngọt', 1, 'chén', 30, 2, 1, 1, 0, 'dinner', 'high'),
+(68, 'Canh bí đao', 1, 'chén', 29, 2, 1, 1, 0, 'dinner', 'high'),
+(69, 'Canh rau dền', 1, 'chén', 22, 2, 0, 0, 0, 'dinner', 'high'),
+(70, 'Nước củ hành', 1, 'chén', 40, 1, 0, 9, 1, 'dinner', 'high'),
+(71, 'Canh bí đao, cà chua', 1, 'chén', 43, 1, 0, 9, 2, 'dinner', 'high'),
+(72, 'Canh xanh bạch ngọc', 1, 'chén', 40, 3, 0, 7, 3, 'dinner', 'high'),
+(73, 'Canh bí xanh thịt nạc', 1, 'chén', 103, 12, 3, 6, 2, 'dinner', 'high');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +138,7 @@ ALTER TABLE `foods`
 -- AUTO_INCREMENT for table `foods`
 --
 ALTER TABLE `foods`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
